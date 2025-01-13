b7ebd61e5f1e73a1f027004e9a8a843445fc3d60
dwiki-mukti <243dwiki.com@gmail.com>
2025-01-13 18:24:14 +0700

    Update | php fpm

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'hello world!']);
 });
 
 Route::get('/now', function () {
